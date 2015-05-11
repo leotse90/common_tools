2795479efeb56c4d9daa1c5e9b4f530d9f56cf23
leotse90 <proleo@foxmail.com>
2015-05-11 13:10:30 +0800

    Add tools for merge dictionary.

--- struct_utils/map_utils.py
+++ struct_utils/map_utils.py
@@ -23,9 +23,31 @@ def order_by_key(src_dict, reverse=False):
     '''
     return [(key, src_dict[key]) for key in sorted(src_dict.keys(), reverse=reverse)]
 
+def merge_two_dicts(dict1, dict2):
+    '''
+        Merge a list of dictionary into a dictionary and return.
+        If more than one dictionary have the same key, the last value will appear in the result.
+    '''
+    return merge_dicts([dict1, dict2])
 
+def merge_dicts(dict_list):
+    '''
+        Merge a list of dictionary into a dictionary and return.
+        If more than one dictionary have the same key, the last value will appear in the result.
+    '''
+    result = {}
+    for tmp_dict in dict_list:
+        result.update(tmp_dict)
+    return result
 
 if __name__ == "__main__":
     test_dict = {"a":3, "c":1, "b":4}
     print order_by_value(test_dict, reverse=False)
     print order_by_key(test_dict, reverse=False)
+    
+    dict1 = {"a":1, "b":2}
+    dict2 = {"a":2}
+    dict3 = {"a":3, "c":5}
+    print merge_dicts([dict1, dict2, dict3])
+    print merge_two_dicts(dict1, dict2)
+    
