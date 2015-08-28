279610b91c1d767670eb8c6630624e4457bb3804
leotse90 <proleo@foxmail.com>
2015-08-28 19:41:55 +0800

    encrypt utils

--- net_utils/encrypt_utils.py
+++ net_utils/encrypt_utils.py
@@ -2,9 +2,9 @@
 
 '''
 encrypt steps:
-1.append APPENDIX to string -> str1;
-2.base64 encrypt -> str2;
-3.base64 encrypt -> str3;
+1.base64 encrypt -> str1;
+2.APPENDIX + str1 -> str2;
+3.base64 encrypt str2 -> str3;
 4.str3 + str2[-RANDOM_NUMS_COUNT:] -> str4
 5.case conversion at every location -> encrypt result;
 
@@ -18,8 +18,8 @@ APPENDIX = u'leotse'
 RANDOM_NUMS_COUNT = 3
 
 def lt_encrypt(source):
-	source += APPENDIX
 	bstr1 = base64.b64encode(source)
+	bstr1 = APPENDIX + bstr1
 	random_appendix = bstr1[-RANDOM_NUMS_COUNT:]
 	bstr2 = base64.b64encode(bstr1)
 	astr = bstr2 + random_appendix
@@ -30,8 +30,8 @@ def lt_decrypt(source):
 	cstr = case_convert(source)
 	astr = cstr[:-RANDOM_NUMS_COUNT]
 	dstr1 = base64.b64decode(astr)
-	dstr2 = base64.b64decode(dstr1)
-	decrypt_str = dstr2[:-len(APPENDIX)]
+	astr = dstr1[len(APPENDIX):]
+	decrypt_str = base64.b64decode(astr)
 	return decrypt_str
 
 def case_convert(str):
